71ba110735ee3a5c2c4762e5e1410b7172a729d3
Dinesh Bala <baladnes@gmail.com>
2022-03-18 10:54:22 +0545

    Setup firebase

--- android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.frontend
+package com.nepsetrade
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:frontend/config/app_links.dart';
@@ -5,7 +6,19 @@ import 'package:frontend/config/app_routes.dart';
 import 'package:frontend/config/app_theme.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCLdtb38lLI3Ezeck3By4UCnBZ-85Nm-Aw",
+      projectId: "nepse-trade",
+      appId: "1:882166498821:android:b0e61c910918f282d2be6c",
+      messagingSenderId: "882166498821",
+      authDomain: "nepse-trade.firebaseapp.com",
+      storageBucket: "nepse-trade.appspot.com",
+      measurementId: "G-Q923NBEKMF",
+    ),
+  );
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: AppTheme.theme().colorScheme.primary,
   ));
